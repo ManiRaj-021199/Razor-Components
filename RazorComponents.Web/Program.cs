cdcd686a238940def20984d54e1b768a77c28215
Mani Raj <maniraj.021199@gmail.com>
2024-05-06 10:38:52 +0530

    ADDED: Project Files

---
+++ RazorComponents.Web/Program.cs
@@ -0,0 +1,26 @@
+using RazorComponents.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if(!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode();
+
+app.Run();
